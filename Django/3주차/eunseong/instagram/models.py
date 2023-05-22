0c526a60460684b718f2b1162fcb3dcab9841457
dong002 <81304010+dong002@users.noreply.github.com>
2023-05-22 11:28:27 +0900

    ::sparkle:: make model

--- Django/3주차/eunseong/instagram/models.py
+++ Django/3주차/eunseong/instagram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    uid = models.CharField(primary_key=True)
+    passwd = models.CharField(null=False)
+    email = models.EmailField(null=False)
+    nickname = models.CharField(max_length=100, null=False)
