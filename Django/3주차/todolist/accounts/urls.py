6b2a3266caa1abe851dcd60099c9271be621704c
dong002 <s21021@gsm.hs.kr>
2023-06-13 21:01:26 +0900

    signup suc

--- Django/3주차/todolist/accounts/urls.py
+++ Django/3주차/todolist/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
-from .views import SignupView
+from .views import SignupView, signup, LoginView
 urlpatterns = [
-    path('signup/', SignupView.as_view())
+    path('signup/', SignupView.as_view()),
+    path('signup_form/', signup),
+    path('login/', LoginView.as_view()),
+    path('login_form', )
 ]
--- Django/3주차/todolist/accounts/views.py
+++ Django/3주차/todolist/accounts/views.py
@@ -1,12 +1,34 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
+
+
+
+@api_view(["GET"])
+def signup(req):
+    return render(req, 'signup.html', {'todos': req.data})
+
+@api_view(["GET"])
+def login(req):
+    return render(req, 'login.html', {'todos': req.data})
 
 class SignupView(APIView):
     def post(self, req):
         user = User.objects.create_user(username=req.data['id'], password=req.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({"Token": token.key, "User": req.data['id']}, content_type="application/json")
+        return render()
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data['id'], password=request.data['password'])
+        if user is not None:
+            token = Token.objects.get(user=user)
+            return Response({"Token": token.key})
+        else:
+            return Response(status=401)
+        # return Response({"Token": token.key, "User": req.data['id']}, content_type="application/json")
